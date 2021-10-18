464a2022ed779795b022068a7f1b743d4b22f2bb
viveking786 <65109405+viveking786@users.noreply.github.com>
2021-10-18 10:16:36 +0530

    changes

--- Src/js/main.js
+++ Src/js/main.js
@@ -6,19 +6,19 @@ $(window).scroll(function(){
 
 $(document).ready(function(){
 
-    $(".filter-button").click(function(){
-        var value = $(this).attr('data-filter');
+    // $(".filter-button").click(function(){
+    //     var value = $(this).attr('data-filter');
         
-        if(value == "all")
-        {
-            $('.filter').show('1000');
-        }
-        else
-        {
-            $(".filter").not('.'+value).hide('3000');
-            $('.filter').filter('.'+value).show('3000');
-        }
-    });
+    //     if(value == "all")
+    //     {
+    //         $('.filter').show('1000');
+    //     }
+    //     else
+    //     {
+    //         $(".filter").not('.'+value).hide('3000');
+    //         $('.filter').filter('.'+value).show('3000');
+    //     }
+    // });
     
     if ($(".filter-button").removeClass("active")) {
 $(this).removeClass("active");
@@ -26,3 +26,5 @@ $(this).removeClass("active");
 $(this).addClass("active");
 
 });
+
+
